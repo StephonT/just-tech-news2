31818ec453b6dfdd4bd91a29634b0946e360826a
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-07-31 20:15:30 -0400

    Used hooks to hash password

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -6,7 +6,7 @@ router.get('/', (req, res) => {
     //Access our User model and run .findAll() method)
     User.findAll({
       //protecting user data
-      attributes: { exclude: ['password'] }
+      // attributes: { exclude: ['password'] }
     })
 
     .then(dbUserData => res.json(dbUserData))
@@ -24,7 +24,7 @@ router.get('/:id', (req, res) => {
             id: req.params.id
         }
     })
-    
+
     .then(dbUserData => {
         if (!dbUserData) {
             res.status(404).json({ message: 'No user found with this id' });
@@ -59,6 +59,7 @@ router.put('/:id', (req, res) => {
 
   // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
   User.update(req.body, {
+    individualHooks: true,
     where: {
       id: req.params.id
     }
