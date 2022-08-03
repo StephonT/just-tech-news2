f92b7e1db4f2326899913288e6482d2cf62bb720
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-08-03 13:08:41 -0400

    finished routes and implemented a many-to-many relationship

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,12 +1,19 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
 const { route } = require('./user-routes');
+const sequelize = require('../../config/connection');
 
 
 // get all posts
 router.get('/', (req, res) => {
     Post.findAll({
-      attributes: ['id', 'post_url', 'title', 'created_at'],
+      attributes: [
+        'id', 
+        'post_url', 
+        'title', 
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
       // Will put the most recent post at the top of the list
       order: [['created_at', 'DESC']],
       include: [
@@ -30,7 +37,13 @@ router.get('/', (req, res) => {
         where: {
             id: req.params.id
         },
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: [
+        'id', 
+        'post_url', 
+        'title', 
+        'created_at'
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
         include: [
             {
                 model: User,
@@ -65,6 +78,19 @@ router.get('/', (req, res) => {
       res.status(500).json(err);
     });
   })
+
+
+  // PUT /api/posts/upvote
+  router.put('/upvote', (req, res) => {
+    // custom static method created in models/Post.js
+    Post.upvote(req.body, { Vote })
+      .then(updatedPostData => res.json(updatedPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  });
+
 
   // Update post
   router.put('/:id', (req, res) => {
