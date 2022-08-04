e00e0c0b1a4d910c86196236cf73def30e2cfbb7
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-08-04 15:32:49 -0400

    populate template with Sequelize data

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
+// creating path for stylesheet
+const path = require('path')
 const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+const sequelize = require('./config/connection');
+
+
+// Setting up Handlebars.js
+
+const hbs = exphbs.create({});
+
+//Setting up handlebars.js
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+//Middleware 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//Allowing access to stylesheet
+app.use(express.static(path.join(__dirname, 'public')));
 
-// turn on routes
-app.use(routes);
+app.use(require('./controllers/'));
+
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
