c3c89d39f3cd3cc62e61e15c8b3ae235ac355cf7
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-08-04 11:18:57 -0400

    updated routes to include comments

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 
+//Instructing the router instance to use these routes
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
 
+//Prefixes for each route
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
